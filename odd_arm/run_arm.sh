89569aedab0e56297b54dee5c8d29d863cefa848
Appw0 <austinpaulpal@gmail.com>
2026-04-14 16:10:54 -0700

    Adjusted ARM launch script

--- odd_arm/run_arm.sh
+++ odd_arm/run_arm.sh
@@ -2,5 +2,4 @@
 source /opt/ros/humble/setup.bash
 source ros_workspace/install/local_setup.bash
 
-ros2 launch arm_robot_moveit_config demo.launch.py --noninteractive
-ros2 run arm_package opencm_command4 --noninteractive # Does this need to be noninteractive?
+ros2 launch arm_robot_moveit_config demo.launch.py --noninteractive & ros2 run arm_package opencm_command4 --noninteractive & wait
